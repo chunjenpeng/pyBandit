454edc2dac247d67ca6d84dfc78182ff864f33cc
PENGChunJen <b00901081@ntu.edu.tw>
2017-05-11 20:43:01 +0800

    complete SPSO2011

--- SPSO2011.py
+++ SPSO2011.py
@@ -1,33 +1,41 @@
+# Source Generated with Decompyle++
+# File: SPSO2011.cpython-34.pyc (Python 3.4)
+
 from copy import deepcopy
 from operator import attrgetter
-import numpy as np 
+import numpy as np
 
 class Point:
-    def __init__( self, fitness, position ):
+    def __init__(self, fitness, position):
         self.fitness = fitness
         self.position = position
 
 
+
 class Particle:
-    def __init__( self, fitness, position, velocity ):
+    def __init__(self, fitness, position, velocity):
         self.current = Point(fitness, position)
         self.velocity = velocity
         self.previous_best = deepcopy(self.current)
         self.previous_best_neighbor = deepcopy(self.current)
 
 
-class PSO:
-    def __init__(self, n_points, dimension, **kwargs):
-        self.dimension = dimension 
-        self.n_points = n_points
-        self.min_bounds = kwargs.get('min_bounds', np.array([0.]*dimension) )
-        self.max_bounds = kwargs.get('max_bounds', np.array([1.]*dimension) )
-        self.config = kwargs.get('config', 'SPSO2011')
 
-        #self.topology = self.ring_topology() 
-        self.topology = self.random_topology() 
-        self.w = 1.0/(2.0*np.log(2))
-        self.c = 0.5 + np.log(2) 
+class PSO:
+    def __init__(self, obj, n_points, dimension, **kwargs):
+
+        self.obj = obj
+        self.dimension = dimension
+        self.n_points = n_points
+        self.min_bounds = kwargs.get('min_bounds', np.array([0] * dimension))
+        self.max_bounds = kwargs.get('max_bounds', np.array([1] * dimension))
+        #self.max_velocity = np.abs(self.max_bounds - self.min_bounds)
+        #self.min_velocity = -(self.max_velocity)
+
+        self.config = kwargs.get('config', 'SPSO2011')
+        self.topology = self.random_topology()
+        self.w = 1.0 / (2.0 * np.log(2.0))
+        self.c = 0.5 + np.log(2.0)
 
         self.iteration = 0
         self.should_terminate = False
@@ -48,137 +56,128 @@ class PSO:
 
             self.swarm.append( Particle(float('Inf'), np.array(x), np.array(v)) )
 
-
+    
     def ring_topology(self):
         ring = []
         l = range(self.n_points)
-        for i, _ in enumerate(l):
-            if i+1 < len(l):
-                ring.append([ l[i-1], l[i], l[i+1] ] )
+        for (i, _) in enumerate(l):
+            if i + 1 < len(l):
+                ring.append([ l[i - 1], l[i], l[i + 1]])
             else:
-                ring.append([ l[i-1], l[i], l[0] ])
+                ring.append([ l[i - 1], l[i], l[0]])
+
         return np.array(ring)
 
-    def random_topology(self, K=3):
+    
+    def random_topology(self, K = 3):
         A = np.eye(self.n_points)
-        v = np.random.randint(self.n_points, size=(self.n_points,K) )
+        v = np.random.randint(self.n_points, size = (self.n_points, K))
         for i in range(v.shape[0]):
-            A[i,v[i]] = 1
-
-        topology = [ [] for i in range(self.n_points) ] 
-        for i, j in np.argwhere(A>0):
+            A[i, v[i]] = 1
+        
+        topology = [ [] for i in range(self.n_points) ]
+        for (i, j) in np.argwhere(A > 0):
             topology[j].append(i)
+        
         return np.array(topology)
 
+    
     def stop(self):
         return self.should_terminate
 
-    def tell(self, positions, fitnesses):
-        for p, position, fitness in zip(self.swarm, positions, fitnesses):
-            assert(position == p.current.position).all()
+    
+    def run(self):
+        self.iteration += 1
 
-            p.current.fitness = fitness
+        for i in np.random.permutation(self.n_points):
+            p = self.swarm[i]
 
+            # Update best of previous_best in neighborhood
+            best_neighbor = min( [self.swarm[neighbor] for neighbor in self.topology[i] ], 
+                                 key = attrgetter('previous_best.fitness') )
+            p.previous_best_neighbor = deepcopy(best_neighbor.previous_best)
+
+            # Update velocity
+            random_position = self.sample_from_hypersphere(p)
+            p.velocity = self.w * p.velocity + random_position - p.current.position
+
+            # Update position
+            p.current.position += p.velocity
+            self.check_confinement(i)
+            
             # Update previous_best
+            p.current.fitness = self.obj(p.current.position)
             if p.current.fitness < p.previous_best.fitness:
                 p.previous_best = deepcopy(p.current)
 
-        # Update previous_best_neighbor
-        for i, p in enumerate(self.swarm):
-            best = i
-            for index in self.topology[i]: 
-                neighbor = self.swarm[index] 
-                if neighbor.previous_best.fitness <  \
-                   self.swarm[best].previous_best.fitness:
-                   best = index
+                # Update previous_best in neighborhood
+                if p.previous_best.fitness < p.previous_best_neighbor.fitness:
+                    p.previous_best_neighbor = deepcopy(p.previous_best)
+                
 
-            p.previous_best_neighbor = deepcopy(self.swarm[best].previous_best)
-
-        #self.print_status()
-
-        # Update global best_fitness
-        current_best_fitness = min( self.swarm, key=attrgetter('current.fitness') ).current.fitness
-
-        if current_best_fitness < self.best_fitness:
-            self.best_fitness = current_best_fitness
-        else:
+        # Update topology if the best know solution has not been improved
+        best = min(self.swarm, key = attrgetter('current.fitness'))
+        if best.current.fitness >= self.best_fitness:
             self.topology = self.random_topology()
+        self.best_fitness = best.current.fitness
+
+        return (best.current.position, best.current.fitness)
+
     
-        self.update()
-        
-
-    def print_status(self):
-        for i in range(self.n_points):
-            print('Particle',i,' neighbors:', self.topology[i])
-            particle = self.swarm[i]
-            x = particle.current.position
-            p = particle.previous_best.position
-            l = particle.previous_best_neighbor.position
-            print('x:', x, particle.current.fitness) 
-            print('p:', p, particle.previous_best.fitness) 
-            print('l:', l, particle.previous_best_neighbor.fitness) 
-            print()
-
-
-
-    def update(self):
-        self.iteration += 1
-        for i in np.random.permutation(self.n_points):
-            p = self.swarm[i]
-            # Update velocity
-            random_position = self.sample_from_hypersphere(p)
-            p.velocity = self.w*p.velocity + random_position - p.current.position
-
-
     def sample_from_hypersphere(self, particle):
         x = particle.current.position
         p = particle.previous_best.position
         l = particle.previous_best_neighbor.position
-
         if (l != p).any():
-            centre = x + self.c/3.0*(p+l-2.0*x) 
+            centre = x + (self.c / 3.0) * (p + l - 2.0 * x)
         else:
-            centre = x + self.c/2.0*(p-x) 
+            centre = x + (self.c / 2.0) * (p - x)
+        r_max = np.linalg.norm(centre - x)
+        r = np.random.uniform(0.0, r_max)
+        v = np.random.uniform(0.0, 1.0, size = self.dimension)
+        v = v * (r / np.linalg.norm(v))
+        return centre + v
 
-        r_max = np.linalg.norm(centre-x) 
-        #r = np.random.uniform(0, r_max)
-        r = np.random.uniform(0, r_max)**(1.0/self.dimension)
-        v = np.random.uniform(0.0, 1.0, size=self.dimension )
-        v = v * (r/np.linalg.norm(v))
-        return centre+v
-
-
-    def ask(self):
-        # Update position 
-        for p in self.swarm:
-            p.current.position += p.velocity
-        self.check_confinement()
-
-        return [ p.current.position for p in self.swarm ]
-
-
-    def check_confinement(self):
+    
+    def check_confinement(self, index):
+        p = self.swarm[index]
         if self.config == 'SPSO2006':
             zeros = np.zeros(self.dimension)
 
-        for p in self.swarm:
-            min_index = np.where(p.current.position < self.min_bounds) 
-            max_index = np.where(p.current.position > self.max_bounds) 
-            
-            if min_index:
-                p.current.position[min_index] = self.min_bounds[min_index]
-                if self.config == 'SPSO2006':
-                    p.velocity[min_index] = zeros[min_index]
-                else: #SPSO 2011
-                    p.velocity[min_index] = -0.5*p.velocity[min_index]
+        min_index = np.where(p.current.position < self.min_bounds)
+        max_index = np.where(p.current.position > self.max_bounds)
 
-            if max_index:
-                p.current.position[max_index] = self.max_bounds[max_index]
-                if self.config == 'SPSO2006':
-                    p.velocity[max_index] = zeros[max_index]
-                else: #SPSO 2011
-                    p.velocity[max_index] = -0.5*p.velocity[max_index]
-            
+        if min_index:
+            p.current.position[min_index] = self.min_bounds[min_index]
+            if self.config == 'SPSO2006':
+                p.velocity[min_index] = zeros[min_index]
+            else:
+                p.velocity[min_index] = -0.5 * p.velocity[min_index]
+
+        if max_index:
+            p.current.position[max_index] = self.max_bounds[max_index]
+            if self.config == 'SPSO2006':
+                p.velocity[max_index] = zeros[max_index]
+            else:
+                p.velocity[max_index] = -0.5 * p.velocity[max_index]
+
+    
+    def print_status(self):
+        for i in range(self.n_points):
+            print('Particle', i, ' neighbors:', self.topology[i])
+            particle = self.swarm[i]
+            x = particle.current.position
+            p = particle.previous_best.position
+            l = particle.previous_best_neighbor.position
+            print('x:', x, particle.current.fitness)
+            print('p:', p, particle.previous_best.fitness)
+            print('l:', l, particle.previous_best_neighbor.fitness)
+            print()
+        
+
+    
+    def get_positions(self):
+        return [ p.current.position for p in self.swarm ]
 
 
 
@@ -249,78 +248,110 @@ def draw_quiver( pso, obj, fig_name, **kwargs ):
     plt.close(fig)
 
 
+class TestSPSO2011:
+
+    def __init__(self, n_points = 12, dimension = 2, function_id = 1, 
+                 max_evaluations = 10000, verbose = False, plot = 0, fig_dir = None):
+
+        print('\nSPSO2011: Solving F%d in %dD with population size %d...\n' % 
+              (function_id, dimension, n_points))
+
+        from optproblems.cec2005 import CEC2005
+        from boundary import Boundary
+
+        self.n_points = n_points
+        self.dimension = dimension
+        self.function_id = function_id - 1
+        self.function = CEC2005(dimension)[self.function_id].objective_function
+        self.max_bounds = Boundary(dimension, self.function_id).max_bounds
+        self.min_bounds = Boundary(dimension, self.function_id).min_bounds
+
+        # Termination parameters
+        self.FE = 0
+        self.iteration = 0
+        self.max_evaluations = max_evaluations * dimension
+        self.termination_error = 1e-08
+        self.should_terminate = False
+        self.optimal_position = CEC2005(dimension)[self.function_id].get_optimal_solutions()[0].phenome
+        self.optimal_fitness = self.function(self.optimal_position)
+        self.best_position = np.zeros_like(self.optimal_position)
+        self.best_fitness = np.inf
+        self.verbose = verbose
+        self.plot = plot
+        self.fig_config = {
+            'fig_dir': fig_dir,
+            'angle': 240,
+            'xlim':[ self.min_bounds[0], self.max_bounds[0] ],
+            'ylim':[ self.min_bounds[1], self.max_bounds[1] ],
+            'optimal': self.optimal_position
+        }
 
 
-def main():
-
-    from optproblems import Individual, Problem
-    from optproblems.cec2005 import CEC2005
-    from optproblems.base import ResourcesExhausted
-    from boundary import Boundary
-
-    DEBUG=True
-    n_points = 12 
-    dimension = 2
-    function_id = 1 
-    max_evaluations = 1e4
-
-    func = CEC2005(dimension)[function_id]
-    optimal_solution = func.get_optimal_solutions()[0]
-    Problem( func.objective_function ).evaluate(optimal_solution)
-    best_fitness = optimal_solution.objective_values 
-    problem = Problem( func.objective_function,  max_evaluations=max_evaluations*dimension )
-    boundary = Boundary(dimension, function_id)
-    fig_config = {
-        'fig_dir': 'test_pso',
-        'angle': 240,
-        'xlim':[ boundary.min_bounds[0], boundary.max_bounds[0] ],
-        'ylim':[ boundary.min_bounds[1], boundary.max_bounds[1] ],
-        'optimal': func.get_optimal_solutions()[0].phenome
-    }
-
-
-    pso = PSO( n_points, dimension, 
-               min_bounds=boundary.min_bounds,
-               max_bounds=boundary.max_bounds,
-             )
+        self.algo = PSO(self.obj, self.n_points, self.dimension, 
+                        min_bounds = self.min_bounds, 
+                        max_bounds = self.max_bounds)
     
-
-    if DEBUG:
-        FE = problem.consumed_evaluations
-        fig_name = ('F%d_%d' % (function_id+1, pso.iteration) )
-        fig_config['fig_title'] = 'F%d, FE=%d' % (function_id+1, FE) 
-        draw_quiver( pso, func.objective_function, fig_name, **fig_config )
-
-    while not pso.stop():
-        positions = pso.ask()
-        solutions = [Individual(position) for position in positions]
-        try:
-            problem.batch_evaluate(solutions)
-        except ResourcesExhausted: break
-
-        pso.tell([p.phenome for p in solutions], [p.objective_values for p in solutions])
+        if self.plot > 0:
+            error = self.best_fitness-self.optimal_fitness
+            fig_name = ('F%d_%d.png' % (self.function_id+1, self.iteration) )
+            self.fig_config['fig_title'] = ('F%d, FE=%d, error=%.2e' % 
+                                            (self.function_id+1, self.FE, error) )
+            draw_quiver( self.algo, self.function, fig_name, **self.fig_config )
 
 
-        if DEBUG:
-            FE = problem.consumed_evaluations
-            error = pso.best_fitness-best_fitness
-            fig_name = ('F%d_%d' % (function_id+1, pso.iteration) )
-            fig_config['fig_title'] = ('F%d, FE=%d, error=%.2e' % (function_id+1, FE, error) )
-            draw_quiver( pso, func.objective_function, fig_name, **fig_config )
-            '''
-            for p in pso.swarm:
-                print(p.fitness, 
-                      p.position, 
-                      p.velocity, 
-                      #p.previous_best.fitness, 
-                      #p.previous_best.position,
-                      #p.previous_best_neighbor.fitness, 
-                      #p.previous_best_neighbor.position,
-                     )
-            '''
-        print('Iteration %d, FE:%d, best fitness: %f, error:%.2e' 
-              % (pso.iteration, FE, pso.best_fitness, error) )
+
+    def obj(self, x):
+        self.FE += 1
+        fitness = self.function(x)
+        if self.FE >= self.max_evaluations:
+            raise Exception('Resources Exhausted!')
+        error = fitness - self.optimal_fitness
+        if error < self.termination_error:
+            self.best_fitness = fitness
+            self.best_position = x
+            raise Exception('Found Optima!')
+        return fitness
+
+    
+    def run(self):
+        while not self.algo.stop():
+            self.iteration += 1
+            
+            try:
+                (self.best_position, self.best_fitness) = self.algo.run()
+            except Exception as e:
+                print(e)
+                break
 
 
-if __name__ == "__main__":
-    main()
+            if self.verbose:
+                error = self.best_fitness - self.optimal_fitness
+                print('Iter:%d, FE:%d, error:%.2e, fitness:%.2f' % 
+                      (self.iteration, self.FE, error, self.best_fitness))
+                #print('position:%s\n' % self.best_position)
+                
+            if self.plot > 0 and self.iteration % self.plot == 0:
+                error = self.best_fitness-self.optimal_fitness
+                fig_name = ('F%d_%d.png' % (self.function_id+1, self.iteration) )
+                self.fig_config['fig_title'] = ('F%d, FE=%d, error=%.2e' % 
+                                                (self.function_id+1, self.FE, error) )
+                draw_quiver( self.algo, self.function, fig_name, **self.fig_config )
+
+
+        error = self.best_fitness - self.optimal_fitness
+        print('Iter:%d, FE:%d, error:%.2e, fitness:%.2f' % 
+              (self.iteration, self.FE, error, self.best_fitness))
+        print('position:%s\n' % self.best_position)
+        return None
+
+
+if __name__ == '__main__':
+    testSPSO2011 = TestSPSO2011(n_points = 12, 
+                                dimension = 2,
+                                function_id = 1, # F1 ~ F25
+                                max_evaluations = 10000,
+                                verbose=True,
+                                plot = 1,
+                                fig_dir = 'test_pso'
+                                )
+    testSPSO2011.run()
